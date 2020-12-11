3d129a98566f0b0072ddf3370dbd91ac1e14f82a
Mirko De Vincentiis <mirko.devin@gmail.com>
2020-12-11 13:52:48 +0100

    Update requirements_installer.ps1
    Now requrements_installer.ps1 download Python version 3.9.0

--- requirements_installer.ps1
+++ requirements_installer.ps1
@@ -5,10 +5,10 @@ $python3Version = Invoke-Expression -Command "python3 -V" 2>&1
 if([string]::IsNullOrEmpty($pythonVersion) -and [string]::IsNullOrEmpty($python3Version))
 {
     Write-Output "No Python version found. Will be installed the version 3.9.0"
-    #[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-    #Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.0/python-3.9.0.exe" -OutFile "c:/temp/python-3.9.0.exe"
-    #c:/temp/python-3.9.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
+    Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.0/python-3.9.0.exe" -OutFile "c:/temp/python-3.9.0.exe"
+    c:/temp/python-3.9.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
     
 }
 else 
